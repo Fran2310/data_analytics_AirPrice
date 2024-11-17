294432044982f81360752ec3584f34c1d0daf47f
Fran2310 <106452242+Fran2310@users.noreply.github.com>
2024-11-16 22:42:38 -0400

    implement seaborn

--- graphics/trends_from_destination_cities.py
+++ graphics/trends_from_destination_cities.py
@@ -1,32 +1,19 @@
 import matplotlib.pyplot as plt
-from palette.generator_palette import extended_palette as palette
+from palette.generator_palette import extended_palette
 import pandas as pd
+import seaborn as sns
 
 data = pd.read_csv('./Clean_Dataset.csv')
 
-import matplotlib.pyplot as plt
-import pandas as pd
-
 avg_prices_by_destination = data.groupby(['destination_city', 'source_city'])['price'].mean().reset_index()
 
-plt.figure(figsize=(10, 10))
-
-# Obtener las ciudades de destino únicas
-destination_cities = avg_prices_by_destination['destination_city'].unique()
-
-# Crear las barras para cada source_city
-for i, source_city in enumerate(avg_prices_by_destination['source_city'].unique()):
-    # Filtrar por source_city específico
-    subset = avg_prices_by_destination[avg_prices_by_destination['source_city'] == source_city]
-    
-    # Crear las barras para source_city, desplazándolas para que no se sobrepongan
-    plt.bar(subset['destination_city'], subset['price'], label=source_city, color=palette[i], width=0.5, align='center')
-
-
-plt.title('Precio promedio del billete por ciudad de destino')
-plt.xlabel('Ciudad de destino')
-plt.ylabel('Precio promedio (INR)')
+plt.figure(figsize=(14, 8))
+sns.barplot(x='destination_city', y='price', hue='source_city', data=avg_prices_by_destination, palette=extended_palette)
+plt.title('Precio medio del billete por ciudad de destino')
+plt.xlabel('Ciudad de Destino')
+plt.ylabel('Precio Promedio (INR)')
 plt.xticks(rotation=45, ha='right')
+plt.grid(True, color=extended_palette[7], linestyle='--', linewidth=0.5)
 plt.legend(title='Ciudad de origen')
-
+plt.tight_layout()
 plt.show()
