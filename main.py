b509dd4f7f9a2d7c9d9dcf314aec243929167021
JoshuaCarrera <cubyt3@gmail.com>
2024-11-18 01:29:50 -0400

    Arreglado rutas imagenes mapa

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ data = pd.read_csv('./data/Clean_Dataset.csv')
 
 palette_colors = ['#00a5c4', '#00596a', '#d5f4f4', '#738484', '#a172a5', '#573e59', '#63529b', '#352c53']
 
-trends_from_destination_cities.graph_gen(data, palette_colors, (10,6))
+# trends_from_destination_cities.graph_gen(data, palette_colors, (10,6))
 
-airline_popularity.graph_gen(data, palette_colors, (8,6))
+# airline_popularity.graph_gen(data, palette_colors, (8,6))
 
-avg_price_airline.graph_gen(data, palette_colors, (6, 6))
+# avg_price_airline.graph_gen(data, palette_colors, (6, 6))
 
 airports_map2.airport_map(airp) #map html
