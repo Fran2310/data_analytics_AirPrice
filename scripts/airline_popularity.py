c743cf280b961d5b26310dbf8a51aa2a969cf3f5
Fran2310 <106452242+Fran2310@users.noreply.github.com>
2024-11-18 19:29:56 -0400

    conclusiones

--- scripts/airline_popularity.py
+++ scripts/airline_popularity.py
@@ -1,6 +1,15 @@
 from .bar_graph import gen_bar
 
 
+"""
+*Mercado: 
+    - Tanto Vistara como Air India se muestran como las opciones mas populares a pesar de presentar los precios mas elevados, consolidándose como actores clave en el mercado.
+    - Dentro del apartado mas económico, Indigo parece tener una ventaja competitiva teniendo una buena popularidad y una buena opcion en el precio promedio de sus boletos colocandose debajo de SpiceJet GO FIRST, pero encima de AirAsia.
+    
+*Relacion:
+    -Posible relacion inversa entre el numero de reservas y el precio (Grafica a desarrollar)
+"""
+
 def graph_gen(data, colors, size=(), img=False):
     data_graph = data['airline'].value_counts()
 
--- scripts/avg_price_airline.py
+++ scripts/avg_price_airline.py
@@ -1,5 +1,6 @@
 from .bar_graph import gen_bar
 
+
 """
 Se observa que:
 
@@ -7,8 +8,12 @@ Se observa que:
     - Vistara y Air India tienen los precios promedio de boletos más altos
     - SpiceJet, GO FIRST, Indigo, y AirAsia tienen precios promedio significativamente más bajos
 
-* Cmpetencia:
+* Competencia:
     - Las aerolíneas más económicas, como SpiceJet, GO FIRST, e Indigo, parecen tener precios similares, sugiriendo competencia directa entre ellas
+
+* Mercado:
+    - Vistara y Air India probablemente ofrecen servicios de mayor calidad o dirigidos a clientes premium, lo que justifica sus precios más elevados.
+    - AirAsia, Indigo, SpiceJet, y GO FIRST parecen competir en el segmento de aerolíneas económicas.
 
 """
 def graph_gen(data, colors, size=(), img=False):
