8ac8fb22fa5ef9b2c39ccdced062d1f1f131baa9
jesus18112003 <ghostdreemurr@gmail.com>
2024-11-18 19:02:33 -0400

    Grafica de matriz y lineal de la coorelacion de variables

---
+++ scripts/corelacion.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from palette.generator_palette import extended_palette as palette
+import pandas as pd
+
+df =  pd.read_csv('./data/Clean_Dataset.csv')
+
+stops_mapping = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3
+}
+
+df['stops'] = df['stops'].map(stops_mapping)
+
+df_numericas = df[['duration', 'days_left', 'price','stops']]
+
+correlacion = df_numericas.corr()
+
+print(correlacion)
+
+#Grafica de calor
+"""plt.figure(figsize=(8, 6))
+sns.heatmap(correlacion, annot=True, cmap='coolwarm', fmt='.2f', cbar=True) 
+
+# Mostrar el gráfico
+plt.title('Matriz de Correlación')
+plt.show()"""
+
+
+#grafica de puntos
+plt.figure(figsize=(8, 6))
+sns.regplot(x='duration', y='price', data=df, scatter_kws={'s': 10}, line_kws={'color': 'red'})
+
+# Título y etiquetas
+plt.title('Relación entre Duración del Vuelo y Precio')
+plt.xlabel('Duración del Vuelo (horas)')
+plt.ylabel('Precio (en la moneda correspondiente)')
+
+# Mostrar el gráfico
+plt.show()
+
+#coorelacion entre aerolineas y precio
