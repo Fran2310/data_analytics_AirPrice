38a13929b600e08f94ed125965a85116e65a73a8
Fran2310 <106452242+Fran2310@users.noreply.github.com>
2024-11-19 00:17:37 -0400

    Update price_1_vs_2_days_depature.py
    add observaciones

--- scripts/price_1_vs_2_days_depature.py
+++ scripts/price_1_vs_2_days_depature.py
@@ -1,5 +1,13 @@
 import matplotlib.pyplot as plt
 
+"""
+* Distrubucion:
+    - La mayor parte de los boletos, tanto para reservas hechas 1 día antes como 2 días antes de la salida, se concentran en el rango de precios bajos de aproximadamente 10,000 a 20,000 INR. Este rango representa la mayor frecuencia en el histograma
+    - A medida que el precio aumenta (más allá de los 30,000 INR), la frecuencia de boletos reservados disminuye significativamente
+    - La mayoría de las reservas realizadas 1 o 2 días antes de la salida se encuentran en rangos de precios similares, lo que indica que los boletos no se encarecen significativamente
+    - El gran volumen de reservas en el rango de precios bajos sugiere que los clientes que reservan con poca antelación priorizan boletos económicos
+"""
+
 
 def graph_gen(data, colors, size=(), img=False):
     data_filtered = data[data['days_left'].isin([1, 2])]
