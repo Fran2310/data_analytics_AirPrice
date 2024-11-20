499df255b8b52b788c591de4c79ea2060f48c110
Fran2310 <106452242+Fran2310@users.noreply.github.com>
2024-11-19 20:56:57 -0400

    Add booking_classes_airline, standard fun for bar_graph_3C

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -5,3 +5,4 @@ from .price_1_vs_2_days_depature import graph_gen
 from .price_days_left import graph_gen
 from .price_class import graph_gen
 from .dist_price_class import graph_gen
+from .booking_classes_airline import graph_gen
