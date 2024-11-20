085a8c109ed809f3c34f062f455719ff760db2a1
jesus18112003 <ghostdreemurr@gmail.com>
2024-11-19 20:48:05 -0400

    Ajuste en la grafica de correlacion, grafica de correlacion dispersa

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from scripts import price_days_left, trends_from_destination_cities, airline_popularity, avg_price_airline, price_1_vs_2_days_depature, price_class, dist_price_class, Crr_re_pr
+from scripts import Correlation_matriz, Correlation_scatter, price_days_left, trends_from_destination_cities, airline_popularity, avg_price_airline, price_1_vs_2_days_depature, price_class, dist_price_class
 from scripts.maps import airports_map2,airports_india
 
 airp = airports_india.airports
@@ -11,7 +11,7 @@ palette_colors = ['#00a5c4', '#00596a', '#d5f4f4', '#738484', '#a172a5', '#573e5
 
 #avg_price_airline.graph_gen(data, palette_colors, (8,6), True)
 
-airline_popularity.graph_gen(data, palette_colors, (8,6), True)
+#airline_popularity.graph_gen(data, palette_colors, (8,6), True)
 
 #price_1_vs_2_days_depature.graph_gen(data, palette_colors, (8,6))
 
@@ -21,9 +21,11 @@ airline_popularity.graph_gen(data, palette_colors, (8,6), True)
 
 #price_class.graph_gen(data, palette_colors, (8,6), True)
 
-dist_price_class.graph_gen(data, palette_colors, (8,6))
+#dist_price_class.graph_gen(data, palette_colors, (8,6))
 
-Crr_re_pr.graph_gen(data,palette_colors, (11,6), True)
+Correlation_matriz.graph_gen(data,palette_colors, (11,6), True)
+
+Correlation_scatter.graph_gen(data,palette_colors, (11,6), True)
 
 #airports_map2.airport_map(airp) #map html
 
--- scripts/__init__.py
+++ scripts/__init__.py
@@ -4,4 +4,6 @@ from .avg_price_airline import graph_gen
 from .price_1_vs_2_days_depature import graph_gen
 from .price_days_left import graph_gen
 from .price_class import graph_gen
+from .Correlation_scatter import graph_gen
+from .Correlation_matriz import graph_gen
 from .dist_price_class import graph_gen
