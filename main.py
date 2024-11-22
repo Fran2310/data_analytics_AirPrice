28ec4f9e4c274e7d0b38d09dcd8e385f34a4c882
Fran2310 <106452242+Fran2310@users.noreply.github.com>
2024-11-22 15:43:18 -0400

    faltaba importacion Price_comp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from scripts import Correlation_matriz, Correlation_scatter, price_days_left, trends_from_destination_cities, airline_popularity, avg_price_airline, price_1_vs_2_days_depature, price_class, dist_price_class, booking_classes_airline, stops_duration_price
+from scripts import Correlation_matriz, Correlation_scatter, price_days_left, trends_from_destination_cities, airline_popularity, avg_price_airline, price_1_vs_2_days_depature, price_class, dist_price_class, booking_classes_airline, stops_duration_price, Price_comp
 from scripts.maps import airports_map,airports_india,airports_popularity_arrives_map, air_routes, airports_popularity_departure_map
 
 airp = airports_india.airports
@@ -30,13 +30,11 @@ palette_colors = ['#00a5c4', '#00596a', '#d5f4f4', '#738484', '#a172a5', '#573e5
 
 #booking_classes_airline.graph_gen(data, palette_colors, (8, 6), True)
 
-#Price_comp.graph_gen(data,palette_colors, (10,6), True)
+Price_comp.graph_gen(data,palette_colors, (10,6), True)
 
 #stops_duration_price.graph_gen_stops(data, palette_colors, (8, 6), True)
 
 #stops_duration_price.graph_gen_duration(data, palette_colors, (8, 6), True)
-
-Price_comp.graph_gen(data,palette_colors, (10,6), True)
 
 #airports_popularity_arrives_map.airports_popularity_arrives_map(airp) #map airports popularity arrives html
 #airports_popularity_departure_map.airports__popularity_departure_map(airp) #map airports popularity departure html
